06a4273f3d0d78ae67240d851f27d34e317df528
wewilliams9999 <williamswallise@gmail.com>
2022-04-05 18:28:14 -0500

    updating List and SQL classes

--- FactoryPatternExercise2/ListDataAccess.cs
+++ FactoryPatternExercise2/ListDataAccess.cs
@@ -6,11 +6,18 @@ namespace FactoryPatternExercise2
 {
     class ListDataAccess : IDataAccess
     {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product() {Name = "Cereal", Price = 2},
+            new Product() {Name = "Milk", Price = 3},
+            new Product() {Name = "Bread", Price = 4},
+            new Product() {Name = "Carrots", Price = 2}
+        };
         List<Product> IDataAccess.LoadData()
         {
             Console.WriteLine($"I am reading data from a list");
             var productList = new List<Product>();
-            return productList;
+            return Products;
         }
 
         void IDataAccess.SaveData()
--- FactoryPatternExercise2/SQLDataAccess.cs
+++ FactoryPatternExercise2/SQLDataAccess.cs
@@ -6,11 +6,18 @@ namespace FactoryPatternExercise2
 {
     class SQLDataAccess : IDataAccess
     {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product() {Name = "Book", Price = 5},
+            new Product() {Name = "Couch", Price = 500},
+            new Product() {Name = "Table", Price = 100},
+            new Product() {Name = "Bench", Price = 75}
+        };
         List<Product> IDataAccess.LoadData()
         {
             Console.WriteLine($"I am reading data from a SQL database");
             var productList = new List<Product>();
-            return productList;
+            return Products;
         }
 
         void IDataAccess.SaveData()
